5405949b9a11db9eee902e1654b58be0dee33d09
Julia Sartirana <jmsartirana@gmail.com>
2020-08-10 15:29:22 -0500

    subo componente CardListPreview y CardMovie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import styled from "styled-components";
 
 import Hero from "../components/Hero";
+import CardListPreview from '../components/CardListPreview'
+import CardMovie from '../components/CardMovie'
 
 import API_KEY from "../data/apiKey";
 
@@ -11,21 +13,43 @@ const StyledContainer = styled.div`
   transform: translateX(-0.7px);
 `;
 
-const Home = () => {
-  const [trendingMovie, setTrendingMovie] = useState({});
+const Container = styled.div `
+  display: flex;
+  flex-direction: column;
+`
+const ContainerFlex = styled.div `
+  display: flex;
+  
+`
 
+const Home = () => {
+  const [trendingMovie, setTrendingMovie] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   useEffect(() => {
     axios
       .get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
       .then((response) => {
         setTrendingMovie(response.data.results[0]);
+        setTrendingMovies(response.data.results.slice(0, 5))
       });
   }, []);
 
   return (
-    <StyledContainer>
-      <Hero data={trendingMovie} />
-    </StyledContainer>
+    <Container>
+      <StyledContainer>
+        <Hero data={trendingMovie} />
+      </StyledContainer>
+    
+      <CardListPreview title= 'Trending Movies'>
+        <ContainerFlex>
+          {
+          trendingMovies.map((movie) => (     
+            <CardMovie data={movie}/>       
+          ))    
+          }
+        </ContainerFlex>   
+      </CardListPreview>
+    </Container>
   );
 };
 
