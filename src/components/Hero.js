96b837a5ba25fe2f82c3dda51a4a9f2423e3f178
Florencia Holzmann <florholzmann@gmail.com>
2020-08-30 19:46:01 -0300

    Transformo trailer en Modal; cambios grales de funcionamiento y estilo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { MdPlayArrow as PlayIcon } from "react-icons/md";
 import Rating from "./Rating";
+import TrailerPage from "../pages/TrailerPage";
 
 const StyledSection = styled.section`
   height: ${(props) => (props.page === "home" ? "70vh" : "90vh")};
@@ -12,8 +13,8 @@ const StyledSection = styled.section`
     props.page === "home" ? "row" : "column-reverse"};
   justify-content: ${(props) =>
     props.page === "home" ? "initial" : "flex-end"};
-  
-  @media(max-width: 950px) {
+
+  @media (max-width: 950px) {
     flex-direction: column-reverse;
     height: auto;
     justify-content: flex-end;
@@ -29,7 +30,7 @@ const StyledContainerInfo = styled.div`
   background-color: black;
   padding: ${(props) => props.page === "secondary" && "4vw 0 0 2vw"};
   padding-left: ${(props) => props.page === "home" && "3vw"};
-  @media(max-width: 950px) {
+  @media (max-width: 950px) {
     width: 90%;
     justify-content: left;
     margin: 6vw 0;
@@ -39,7 +40,7 @@ const StyledContainerInfo = styled.div`
 const Container = styled.div`
   padding-right: ${(props) => (props.page === "home" ? "0" : "5vw")};
   max-width: ${(props) => props.page === "secondary" && "30%"};
-  @media(max-width: 950px) {
+  @media (max-width: 950px) {
     padding-left: ${(props) => props.page === "secondary" && "3vw"};
     max-width: ${(props) => props.page === "secondary" && "100%"};
   }
@@ -52,8 +53,8 @@ const BackgrdImgContainer = styled.div`
   background-size: cover;
   box-shadow: ${(props) =>
     props.page === "home" ? "inset 50px -20px 60px 60px #000" : "none"};
-  
-  @media(max-width: 950px) {
+
+  @media (max-width: 950px) {
     width: 100vw;
     height: 18rem;
     box-shadow: inset 0px -20px 30px 6px #000;
@@ -72,7 +73,7 @@ const StyledTitleLink = styled(Link)`
   &:hover {
     color: ${(props) => (props.page === "home" ? "#2196f3" : "#fafafa")};
   }
-  @media(max-width: 950px) {
+  @media (max-width: 950px) {
     font-size: 2.5rem;
     line-height: 6vh;
   }
@@ -86,7 +87,7 @@ const StyledDescription = styled.p`
   width: ${(props) => (props.page === "home" ? "auto" : "50%")};
   margin: ${(props) => props.page === "secondary" && "0"};
 
-  @media(max-width: 950px) {
+  @media (max-width: 950px) {
     display: none;
   }
 `;
@@ -98,29 +99,28 @@ const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-right: .7vw;
+  padding-right: 0.7vw;
   transition: 0.2s;
+  width: ${(props) => props.page === "home" && "50%"};
+
   &:hover {
     cursor: pointer;
     background-color: #2f2f2f;
     border: solid #2f2f2f;
     transition: 0.2s;
   }
+
+  @media (max-width: 950px) {
+    display: none;
+  }
 `;
+
 const StyledPlayIcon = styled(PlayIcon)`
   font-size: 1.3vw;
   color: #fff;
   margin-right: 0.7vw;
 `;
-const StyledTrailerLink = styled(Link)`
-  text-decoration: none;
-  width: 13vw;
-  margin-top: 2vw;
 
-  @media(max-width: 950px) {
-    display: none;
-  }
-`;
 const Text = styled.p`
   color: #fff;
   font-family: roboto;
@@ -130,6 +130,10 @@ const Text = styled.p`
 
 const Hero = ({ data, media_type, page = "home" }) => {
   const { title, overview, backdrop_path, vote_average, name, id } = data;
+  const [isTrailerSelected, setIsTrailerSelected] = useState(false);
+
+  const handleClick = () => setIsTrailerSelected(true);
+  const handleClickClose = () => setIsTrailerSelected(false);
 
   return (
     <StyledSection page={page}>
@@ -139,27 +143,32 @@ const Hero = ({ data, media_type, page = "home" }) => {
             {title || name}
           </StyledTitleLink>
           <Rating rating={vote_average} page={page} />
-          {page === "secondary" &&
-            <StyledTrailerLink to={`/video/${media_type}/${id}`}>
-              <Button>
-                <StyledPlayIcon />
-                <Text>Watch Trailer</Text>
-              </Button>
-            </StyledTrailerLink>}
-        </Container>
-        <StyledDescription page={page}>{overview}</StyledDescription>
-        {page === "home" &&
-          <StyledTrailerLink to={`/video/${media_type}/${id}`}>
-            <Button>
+          {page === "secondary" && (
+            <Button onClick={handleClick}>
               <StyledPlayIcon />
               <Text>Watch Trailer</Text>
             </Button>
-          </StyledTrailerLink>}
+          )}
+        </Container>
+        <StyledDescription page={page}>{overview}</StyledDescription>
+        {page === "home" && (
+          <Button onClick={handleClick} page={page}>
+            <StyledPlayIcon />
+            <Text>Watch Trailer</Text>
+          </Button>
+        )}
       </StyledContainerInfo>
       <BackgrdImgContainer
         page={page}
         img={`https://image.tmdb.org/t/p/original${backdrop_path}`}
       ></BackgrdImgContainer>
+      {isTrailerSelected && (
+        <TrailerPage
+          media={media_type}
+          id={id}
+          onHandleClick={handleClickClose}
+        />
+      )}
     </StyledSection>
   );
 };
